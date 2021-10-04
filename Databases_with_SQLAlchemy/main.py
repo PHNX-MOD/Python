58e98b23deabd479c26f5b8900b75bc801743ad3
modleo <40522163+PHNX-MOD@users.noreply.github.com>
2021-10-04 12:49:02 +0000

    create a base class

--- Databases_with_SQLAlchemy/main.py
+++ Databases_with_SQLAlchemy/main.py
@@ -1,4 +1,15 @@
 from sqlalchemy import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
+"""
+Class = User
+id int
+username str
+email str
+date_created datetime"""
+
+class user(Base):
+  __tablename__ = 'users'
+  id=
