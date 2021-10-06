9df7b6e3355d929e7edc21529e7cbe47ef817dc5
PHNX-MOD <modith.hadya@gmail.com>
2021-10-06 16:26:07 +0200

    sqlite site db create engine

--- Databases_with_SQLAlchemy/main.py
+++ Databases_with_SQLAlchemy/main.py
@@ -13,11 +13,12 @@ engine = create_engine(connection_string, echo=True)
 class User(Base):
   __tablename__ = 'users'
   id=Column(Integer(), primary_key=True)
-  username=Column(String(25),nullable=False, unique=True)
-  email=Column(String(80),unique=True, nullable= False)
+  username=Column(String(25), nullable=False, unique=True)
+  email=Column(String(80), unique=True, nullable=False)
   date_created = Column(DateTime(), default=datetime.utcnow)
 
   def __repr__(self):
     return f"<User username{self.username} email={self.email}>"
 
 new_user = User(id=1, username="test1", email="test1@gmail.com")
+print(new_user)
