7af835fbf5e162d5d3de568e3576ca4d2dba92ad
modleo <40522163+PHNX-MOD@users.noreply.github.com>
2021-10-10 17:45:03 +0000

    create users using importing csv

--- Databases_with_SQLAlchemy/create_users.py
+++ Databases_with_SQLAlchemy/create_users.py
@@ -3,15 +3,31 @@ import csv
 import json 
 
 
-def import_data_from_csv("csvFilePath"):
 
-  with open()
-
-
-
-local_session=Session(bind=engine)
+"""local_session=Session(bind=engine)
 
 new_user = User(id=1, username="test1", email="test1@gmail.com")
 
 local_session.add(new_user)
-local_session.commit()
+local_session.commit()"""
+
+ 
+data =[]
+
+with open(r'TestData.csv',encoding='utf-8') as csvf:
+  csvreader = csv.DictReader(csvf)
+
+  for row in csvreader:
+    data.append(row)
+
+jsonString = json.dumps(data, indent=4)
+
+
+
+for user in jsonString:
+  new_user = User(username=user["username"], email=user["email"])
+
+
+
+
+
