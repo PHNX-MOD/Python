17caa7e264e9194360229a28c31e6b2955308deb
PHNX-MOD <modith.hadya@gmail.com>
2021-10-11 13:54:08 +0200

    users added to sqlite

--- Databases_with_SQLAlchemy/create_users.py
+++ Databases_with_SQLAlchemy/create_users.py
@@ -1,18 +1,9 @@
-from main import User,Session,engine
+from main import User, Session, engine
 import csv
-import json 
 
 
-
-"""local_session=Session(bind=engine)
-
-new_user = User(id=1, username="test1", email="test1@gmail.com")
-
-local_session.add(new_user)
-local_session.commit()"""
-
- 
 data =[]
+local_session=Session(bind=engine)
 
 with open(r'TestData.csv',encoding='utf-8') as csvf:
   csvreader = csv.DictReader(csvf)
@@ -20,14 +11,11 @@ with open(r'TestData.csv',encoding='utf-8') as csvf:
   for row in csvreader:
     data.append(row)
 
-jsonString = json.dumps(data, indent=4)
-
-
-
-for user in jsonString:
+for user in data:
   new_user = User(username=user["username"], email=user["email"])
+  local_session.add(new_user)
+  local_session.commit()
 
-
-
+  print(f"added",{user["username"]})
 
 
